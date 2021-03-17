c89d01c21ea4aba580a9bebb8c48eeec7bd33b0a
YanTingLin <H34066131@gs.ncku.edu.tw>
2021-03-17 10:57:35 +0800

    fix: 修正baseurl造成的網址match不正確問題

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import { Provider } from 'react-redux'
 import rootReducer from './store/reducers/rootReducer'
 
 const store = createStore(rootReducer)
+process.env.BASE_URL = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : ''
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename={process.env.BASE_URL}>
         <App />
       </Router>
     </Provider>
