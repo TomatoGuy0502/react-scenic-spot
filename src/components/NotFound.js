c5584b50fea2760f8d34c311d5ff03eb70f781ea
YanTingLin <H34066131@gs.ncku.edu.tw>
2021-04-04 16:39:35 +0800

    feat: 修改404頁面內容

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
-  const location = useLocation()
-
   return (
-    <div>
-      <h3>
-        Oops! 找不到 <code>{location.pathname}</code>
-      </h3>
+    <div className="h-100 d-flex flex-column justify-content-center text-center">
+      <div>
+        <h1 className="mb-5">Oops！好像迷路了</h1>
+        <Link to="/scenicSpot" className="btn btn-lg btn-info">
+          回去瀏覽景點
+        </Link>
+      </div>
     </div>
   )
 }
